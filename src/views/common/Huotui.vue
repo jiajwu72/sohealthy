8818c53ccceebef39ec38cd1653ad12b202efce7
jiajun wu <72jiajunwu@gmail.com>
2021-02-02 19:04:48 +0100

    update again

--- src/views/common/Huotui.vue
+++ src/views/common/Huotui.vue
@@ -3,26 +3,276 @@
   <!-- <div class="head">
 
   </div> -->
+  <div>
+    <v-img
+      src="/images/火腿/Front_logo.jpg"
+    ></v-img>
+  </div>
   <div class="content">
-    <v-icon dark right>mdi-checkbox-marked-circle</v-icon>
-
-    <b-container class="">
-      <!-- <div class="" v-for="(item, index) in huotui" :key="index">
-        <h2>{{item.name}}</h2>
-        <b-row v-for="(el, index) in item.elemnts" :key="index">
+    <b-container>
+      <div class="Block">
+        <p>
+          每个故事都有他的起源，来探访我们吧
+        </p>
+        <p>
+          了解我们是如何打造世界独一无二的火腿品牌
+        </p>
+      </div>
+      <div class="Block">
+        <b-row>
           <b-col md="6">
             <v-img
-            :src="el.img"
+              src="/images/火腿/CAVE.jpg"
             ></v-img>
           </b-col>
-          <b-col md="6" class="text_block">
-            {{el.desc}}
+          <b-col md="6" style="display:flex;">
+            <div style="margin:0 auto;">
+              <p class="title titleSize">
+                  J CINCO JOTAS的传奇精髓
+              </p>
+              <p style="margin-top:50px;">
+                游览我们的地窖，深入了解1879年以来坚持传统的制作过程，整个过程历时五年以上。
+              </p>
+            </div>
           </b-col>
         </b-row>
-        <br>
-      </div> -->
+      </div>
+      <div class="Block">
+        <b-row>
+          <b-col md="6" style="display:flex;">
+            <div style="margin:0 auto;">
+              <p class="title titleSize">
+                  精湛的手工艺
+              </p>
+              <p style="margin-top:50px;">
+                5J CINCO JOTAS代代传承手工腌制工艺，从我们工匠大师学习每一片火腿是如何精心腌制的。
+              </p>
+            </div>
+          </b-col>
+          <b-col md="6">
+            <v-img
+              src="/images/火腿/22.jpg"
+            ></v-img>
+          </b-col>
+        </b-row>
+      </div>
 
-      <h1>西班牙 5J火腿</h1>
+      <div class="Block">
+        <b-row>
+          <b-col md="6">
+            <v-img
+              src="/images/火腿/TB2LYpNjiRnpuFjSZFCXXX2DXXa_!!479954274.jpg"
+            ></v-img>
+          </b-col>
+          <b-col md="6" style="display:flex;">
+            <div style="margin:0 auto;">
+              <p class="title titleSize">
+                  品尝盛宴
+              </p>
+              <p style="margin-top:50px;">
+                品味享受西班牙的国宝，口中享受一份特别的味道，每一片火腿都是出自我们切割大师之手。
+              </p>
+            </div>
+          </b-col>
+        </b-row>
+      <div class="divider">
+        <p class="title">
+            我们的产品
+        </p>
+      </div>
+      <b-row>
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-1.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-2.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-3.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-4.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-5.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-6.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-7.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-8.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-14.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-12.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/htx2.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-13.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-10.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+        <b-col md="3" id="wrapper">
+          <div id="elDiv">
+            <v-img
+              src="/images/火腿/new-product-image-9.jpg"
+            ></v-img>
+          </div>
+          <p class="title titleSizeMini">
+            橡果喂养100%伊比利亚火腿 后腿薄片
+          </p>
+          <p>
+            70克薄片装
+          </p>
+        </b-col>
+      </b-row>
+
+
+      </div>
+      <!-- <h1>西班牙 5J火腿</h1>
       <b-row>
         <b-col md="6">
           <v-img
@@ -52,14 +302,12 @@
           ></v-img>
         </b-col>
         <b-col md="6">
-          <!-- <v-img
-          src="/images/火腿/TB2LYpNjiRnpuFjSZFCXXX2DXXa_!!479954274.jpg"
-          ></v-img> -->
+
           <div class="text_block">
             <p>
             好的伊比利亚火腿因选用橡树果喂养而充满果香与油脂的香气，丝毫不会过咸，可以直接食用。需注意的是，不要用刀叉，直接用手把切好的火腿片送入口中，才能享受到细腻的肉质和毫不油腻的脂肪部位。
-<br>
-除了口感鲜美，伊比利亚火腿的营养价值也很高。据最新研究表明，它不仅富含人体组织所需的蛋白质和维他命B1、B6及B12，还含有对人体神经系统和大脑有益的叶酸和对骨骼有益的铜、铁和磷等矿物质。
+            <br>
+            除了口感鲜美，伊比利亚火腿的营养价值也很高。据最新研究表明，它不仅富含人体组织所需的蛋白质和维他命B1、B6及B12，还含有对人体神经系统和大脑有益的叶酸和对骨骼有益的铜、铁和磷等矿物质。
             </p>
           </div>
         </b-col>
@@ -67,42 +315,23 @@
       <br>
       <h1>我们的产品</h1>
       <b-row>
-        <!-- <b-col md="6">
-          <v-img
-          src="/images/火腿/mmexport1498589569141.jpg"
-          ></v-img>
-        </b-col> -->
-
         <b-col md="6">
-
           <v-img
           src="/images/火腿/_TUBE LOMO 500Gr.jpg"
           style="width:40%;"
           ></v-img>
-          <!-- <div class="text_block">
-            <p>
-              谷饲林地散养伊比利亚黑猪火腿，成品为白色标签
-            </p>
-
-          </div> -->
         </b-col>
         <b-col md="6">
           <v-img
           src="/images/火腿/_JAMBON 70Gr.jpg"
           ></v-img>
-          <!-- <div class="text_block">
-            <p>
-              谷饲林地散养伊比利亚黑猪火腿，成品为白色标签
-            </p>
-
-          </div> -->
         </b-col>
         <b-col md="6">
           <v-img
           src="/images/火腿/_LOMO 70Gr.jpg"
           ></v-img>
         </b-col>
-      </b-row>
+      </b-row> -->
 
 
     </b-container>
@@ -134,42 +363,6 @@ export default{
     }
   },
   created() {
-    // var cat1={
-    //   name:"西班牙 5J火腿",
-    //   elemnts:[
-    //     {
-    //       img:"",
-    //       desc:"",
-    //     },
-    //     {
-    //       img:"",
-    //       desc:"",
-    //     },
-    //     {
-    //       img:"",
-    //       desc:"",
-    //     }
-    //   ]
-    // };
-    // var cat1={
-    //   name:"火腿-Cebo de campo",
-    //   elemnts:[
-    //     {
-    //       img:"",
-    //       desc:"",
-    //     },
-    //     {
-    //       img:"",
-    //       desc:"",
-    //     },
-    //     {
-    //       img:"",
-    //       desc:"",
-    //     }
-    //   ]
-    // };
-
-
 
   }
 
@@ -207,6 +400,11 @@ export default{
   margin-top: 15px;
 }
 
+.Block{
+  padding: 50px 0px 50px 0px;
+  border-bottom: 1px black solid;
+}
+
 h1{
   font-family: "Cardo", serif;
   color: #A1907C;
@@ -215,6 +413,41 @@ h1{
 p{
   color: #4A4A4A;
   font-family: "Cardo", serif;
+  text-align: center;
+
+}
+
+.title{
+  font-family: Raleway, 新宋体, NSimSun, 华文黑体, STHeiti, sans-serif;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #b69f67;
+}
+
+.titleSize
+{
+  font-size: 30px;
+}
+
+.titleSizeMini
+{
+  font-size: 18px;
+}
+
+.divider{
+  width: 100%;
+  background: #000;
+  min-height: 80px;
+  padding: 20px 0px;
+  margin-top: 80px;
+}
+#wrapper {
+  position:relative;
+  text-align: center;
+}
+#elDiv {
+  width: 100%;
+  display: inline-block;
 }
 
 </style>
